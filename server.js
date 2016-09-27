447ba43e833f5e368af3263fb3d5ec06f30debea
pentel13 <bmorelli25@gmail.com>
2016-09-27 15:23:40 -0700

    Adding my own Middleware
    app level middleware vs. route level middleware
    app.use vs 2nd input

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ var express = require('express');
 var app = express();
 
 var PORT = 3000;
+var middleware = {
+  requireAuthentication: function (req, res, next) { //route level middleware
+    console.log('private route hit!');
+    next();
+  },
+  logger: function (req, res, next) { // app level middleware
+    var date = new Date().toString();
+    console.log('Request: ' + req.method + ' ' + req.originalUrl + 'on: ' + date);
+    next();
+  }
+};
 
-app.get('/about', function (req, res) {
+// app.use(middleware.requireAuthentication); app level middleware
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication,function (req, res) {
   res.send('About Us');
 });
 
